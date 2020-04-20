81d7ffc2d28746b9cb81ad6a9ced17a098ca096e
e <xiexuhong@superbgrace.com>
2020-04-20 14:34:15 +0800

    资产l列表接口

--- src/script/api.js
+++ src/script/api.js
@@ -1,5 +1,8 @@
 import { post, get } from './http';
 
+
 export const getCountry = url => import(`@/assets/json/country.${url}.json`);
 
 export const login = data => post('/index.php?act=user&op=login', data);
+
+export const getAssetList = data =>get('/index.php?act=asset&op=list',data);
