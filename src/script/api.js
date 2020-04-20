4659254307b74cc837fa8c3e05d87be187b4f9f6
aaa <zhangyifan@superbgrace.com>
2020-04-20 10:40:04 +0800

    登录接口

--- src/script/api.js
+++ src/script/api.js
@@ -1,1 +1,5 @@
+import { post, get } from './http';
+
 export const getCountry = url => import(`@/assets/json/country.${url}.json`);
+
+export const login = data => post('/index.php?act=user&op=login', data);
